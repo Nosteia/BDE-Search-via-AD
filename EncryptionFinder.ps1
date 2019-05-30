a8acfb2246d7610713b03c29f644a3f85a66a03f
Nosteia <jharrell654@gmail.com>
2019-05-30 14:40:47 -0400

    Add files via upload

---
+++ EncryptionFinder.ps1
@@ -0,0 +1,29 @@
+﻿$OU = "AD INFO HERE"
+$computers = Get-ADComputer -Filter * -SearchBase $OU | Select-Object Name | Foreach {$_.Name}
+$fileDump = "C:\scripts\Dump"
+
+foreach ($computer in $computers)
+    {
+    $PingResult = Test-Connection -ComputerName $computer -Count 1 -Quiet
+		    If($PingResult)
+		    {
+                $bdeStatus = Get-WmiObject -Class Win32_EncryptableVolume -Namespace "root\cimv2\security\MicrosoftVolumeEncryption" -ComputerName $computer -ErrorAction SilentlyContinue 
+
+                If($bdeStatus.ProtectionStatus -ne 0)
+				{
+                    Write-Host "$computer is protected."
+                    Out-File -append -FilePath $fileDump\encrypted.txt -InputObject $computer
+                    }
+                    Else
+                    {
+                    Write-Host "$computer is not protected"
+                    Out-File -append -FilePath $fileDump\unencrypted.txt -InputObject $computer
+                    }
+                    $bdeStatus = $null
+            }
+            Else
+            {
+                Write-Host "$computer isn't responding. Skipping..."
+                Out-File -append -FilePath $fileDump\skipped.txt -InputObject $computer
+            }
+    }
